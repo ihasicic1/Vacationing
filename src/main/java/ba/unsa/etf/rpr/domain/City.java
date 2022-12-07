e05f73155edcdc055fe3b9a137b5b146af519eba
ihasicic1 <ihasicic1@etf.unsa.ba>
2022-12-07 21:07:30 +0100

    implemented setters and getters in city class

--- src/main/java/ba/unsa/etf/rpr/domain/City.java
+++ src/main/java/ba/unsa/etf/rpr/domain/City.java
@@ -7,4 +7,12 @@ package ba.unsa.etf.rpr.domain;
 
 public class City {
     private int city_id;
+
+    public int getCity_id() {
+        return city_id;
+    }
+
+    public void setCity_id(int city_id) {
+        this.city_id = city_id;
+    }
 }
