d10b2cbc421c5d31d8863740d1b91c125937f4c7
ihasicic1 <ihasicic1@etf.unsa.ba>
2022-12-11 22:10:45 +0100

    implemented getById method in CityDaoSQLImpl

--- src/main/java/ba/unsa/etf/rpr/dao/CityDaoSQLImpl.java
+++ src/main/java/ba/unsa/etf/rpr/dao/CityDaoSQLImpl.java
@@ -1,14 +1,35 @@
 package ba.unsa.etf.rpr.dao;
 
 import ba.unsa.etf.rpr.domain.City;
+import ba.unsa.etf.rpr.domain.Tour;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.util.List;
 
 public class CityDaoSQLImpl implements CityDao{
-
+    private Connection conn;
     @Override
     public City getById(int id) {
-        return null;
+        String query = "SELECT * FROM Cities WHERE city_id = ?";
+        try{
+            PreparedStatement stmt = this.conn.prepareStatement(query);
+            stmt.setInt(1, id);
+            ResultSet rs = stmt.executeQuery();
+            if(rs.next()){
+                City city = new City();
+                city.setCity_id(rs.getInt("city_id"));
+                city.setCity_name(rs.getString("city_name"));
+                rs.close();
+                return city;
+            }else{
+                return null;
+            }
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
     }
 
     @Override
