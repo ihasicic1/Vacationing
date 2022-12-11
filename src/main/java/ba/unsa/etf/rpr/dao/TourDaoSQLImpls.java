be2aa877d0419f7ff0153729c87759e87e2ac370
ihasicic1 <ihasicic1@etf.unsa.ba>
2022-12-11 20:55:11 +0100

    added methods into TourDaoSQLImpls

--- src/main/java/ba/unsa/etf/rpr/dao/TourDaoSQLImpls.java
+++ src/main/java/ba/unsa/etf/rpr/dao/TourDaoSQLImpls.java
@@ -1,4 +1,47 @@
 package ba.unsa.etf.rpr.dao;
 
-public class TourDaoSQLImpls {
+import ba.unsa.etf.rpr.domain.Tour;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.util.List;
+
+public class TourDaoSQLImpls implements TourDao {
+    private Connection conn;
+
+    @Override
+    public Tour getById(int id) {
+        return null;
+    }
+
+    @Override
+    public Tour add(Tour item) {
+        return null;
+    }
+
+    @Override
+    public Tour update(Tour item) {
+        return null;
+    }
+
+    @Override
+    public void delete(int id) {
+
+    }
+
+    @Override
+    public List<Tour> getAll() {
+        return null;
+    }
+
+    @Override
+    public List<Tour> searchById(int id) {
+        return null;
+    }
+
+    @Override
+    public List<Tour> searchByCity(String name) {
+        return null;
+    }
 }
