4a422cf8b3f939f2ba6158e582558dfd42b2fecd
ihasicic1 <ihasicic1@etf.unsa.ba>
2022-12-11 01:00:35 +0100

    added setters and getters to class Tour

--- src/main/java/ba/unsa/etf/rpr/domain/Tour.java
+++ src/main/java/ba/unsa/etf/rpr/domain/Tour.java
@@ -1,8 +1,29 @@
 package ba.unsa.etf.rpr.domain;
 
 /**
+ * class for all available tours in our travel agency
  * @author Ilhan Hasicic
  */
 
 public class Tour {
+    private int tour_id;
+    private City city_id;
+
+    public int getTour_id() {
+        return tour_id;
+    }
+
+    public void setTour_id(int tour_id) {
+        this.tour_id = tour_id;
+    }
+
+    public City getCity_id() {
+        return city_id;
+    }
+
+    public void setCity_id(City city_id) {
+        this.city_id = city_id;
+    }
+
+
 }
