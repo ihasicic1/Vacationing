df37c83c9e912155b5db68804833ac719fbcdff8
ihasicic1 <ihasicic1@etf.unsa.ba>
2022-12-18 12:47:15 +0100

    made configuration for javafx app

--- src/main/java/ba/unsa/etf/rpr/App.java
+++ src/main/java/ba/unsa/etf/rpr/App.java
@@ -1,9 +1,23 @@
 package ba.unsa.etf.rpr;
 
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+import ba.unsa.etf.rpr.dao.*;
+import ba.unsa.etf.rpr.domain.Booking;
+import ba.unsa.etf.rpr.domain.City;
+import ba.unsa.etf.rpr.domain.Customer;
+import ba.unsa.etf.rpr.domain.Tour;
+import javafx.application.Application;
+import javafx.stage.Stage;
+import java.io.IOException;
+import java.util.List;
+
+
+public class App extends Application {
+    public static void main( String[] args ) throws IOException {
+
+    }
+
+    @Override
+    public void start(Stage stage) throws Exception {
+
     }
 }
