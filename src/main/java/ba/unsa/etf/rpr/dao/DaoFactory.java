e46fc432508314f6711bd7cbfa2e65e77f0bb219
ihasicic1 <ihasicic1@etf.unsa.ba>
2022-12-25 21:55:41 +0100

    added factory methods

---
+++ src/main/java/ba/unsa/etf/rpr/dao/DaoFactory.java
@@ -0,0 +1,31 @@
+package ba.unsa.etf.rpr.dao;
+
+/**
+ *Factory method for singleton implementation of DAOs
+ */
+public class DaoFactory {
+    private static final CustomerDao customerDao = new CustomerDaoSQLImpl();
+    private static final BookingDao bookingDao = new BookingDaoSQLImpl();
+    private static final TourDao tourDao = new TourDaoSQLImpl();
+    private static final CityDao cityDao = new CityDaoSQLImpl();
+
+    private DaoFactory(){
+
+    }
+
+    public static CustomerDao customerDao(){
+        return customerDao;
+    }
+
+    public static BookingDao bookingDao(){
+        return bookingDao;
+    }
+
+    public static TourDao tourDao(){
+        return tourDao;
+    }
+
+    public static CityDao cityDao(){
+        return cityDao;
+    }
+}
