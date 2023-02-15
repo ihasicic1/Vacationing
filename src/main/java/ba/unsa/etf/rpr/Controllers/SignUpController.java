87812004de551597996b10944042ffec2ff372cc
ihasicic1 <ihasicic1@etf.unsa.ba>
2023-02-15 14:39:05 +0100

    implemented validation of input data in signUpAction method

--- src/main/java/ba/unsa/etf/rpr/Controllers/SignUpController.java
+++ src/main/java/ba/unsa/etf/rpr/Controllers/SignUpController.java
@@ -2,6 +2,8 @@ package ba.unsa.etf.rpr.Controllers;
 
 import ba.unsa.etf.rpr.dao.CustomerDaoSQLImpl;
 import ba.unsa.etf.rpr.dao.DaoFactory;
+import ba.unsa.etf.rpr.domain.Customer;
+import ba.unsa.etf.rpr.exceptions.MyException;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
@@ -10,6 +12,7 @@ import javafx.scene.control.*;
 import javafx.stage.Stage;
 
 import java.io.IOException;
+import java.util.List;
 import java.util.Optional;
 
 import static javafx.scene.layout.Region.USE_COMPUTED_SIZE;
@@ -17,15 +20,29 @@ import static javafx.scene.layout.Region.USE_COMPUTED_SIZE;
 public class SignUpController {
     public Button xButton;
     public Button minimizeButton;
-    public Hyperlink loginHyper;
     public TextField firstNameId;
     public TextField lastNameId;
     public TextField emailId;
     public PasswordField passwordId;
     public TextField phoneNumberId;
+    public Hyperlink loginHyper;
+    public Label invalidFirstNameId;
+    public Label invalidLastNameId;
+    public Label invalidEmailId;
+    public Label invalidPasswordId;
+    public Label invalidNumberId;
+    public RadioButton maleButton;
+    public RadioButton femaleButton;
+    public Button signUpButton;
+    public ToggleGroup SpolOdabir;
 
-   //private CustomerDaoSQLImpl dao = (CustomerDaoSQLImpl) DaoFactory.customerDao();
-   private DaoFactory dao = new DaoFactory();
+    List<Customer> customers = DaoFactory.customerDao().getAll();
+
+    public SignUpController() throws MyException {
+    }
+
+    //private CustomerDaoSQLImpl dao = (CustomerDaoSQLImpl) DaoFactory.customerDao();
+   //private DaoFactory dao = new DaoFactory();
 
 
     public void exitAction(ActionEvent actionEvent) {
@@ -47,7 +64,7 @@ public class SignUpController {
     }
 
     public void changeToLogin(ActionEvent actionEvent) throws IOException {
-        Stage s = (Stage) firstNameId.getScene().getWindow();
+        Stage s = (Stage) emailId.getScene().getWindow();
         s.close();
         Stage stage = new Stage();
         Parent root = FXMLLoader.load(getClass().getResource("/fxml/login.fxml"));
@@ -56,5 +73,67 @@ public class SignUpController {
         stage.setResizable(false);
         //primaryStage.initStyle(StageStyle.UNDECORATED);
         stage.show();
+    }
+
+    public void pickMaleAction(ActionEvent actionEvent) {
+
+    }
+
+    public void pickFemaleAction(ActionEvent actionEvent) {
+    }
+
+    public void signUpAction(ActionEvent actionEvent) {
+        if(firstNameId.getText().isEmpty()){
+            invalidFirstNameId.setText("First name field can not be empty!");
+        } else{
+            invalidFirstNameId.setText("");
+        }
+
+        if(lastNameId.getText().isEmpty()){
+            invalidLastNameId.setText("Last name field can not be empty!");
+        } else{
+            invalidLastNameId.setText("");
+        }
+
+        if(emailId.getText().isEmpty()){
+            invalidEmailId.setText("Email field can not be empty!");
+        } else {
+            boolean sameEmail = false;
+            for(Customer cust: customers){
+                if(cust.getEmail().equals(emailId.getText())){
+                    sameEmail = true;
+                    break;
+                }
+            }
+            if(sameEmail) invalidEmailId.setText("Email is already taken!");
+            if(!sameEmail && emailId.getLength() < 6) invalidEmailId.setText("Email must contain at least 6 characters!");
+            else invalidEmailId.setText("");
+        }
+
+        if(passwordId.getText().isEmpty()){
+            invalidPasswordId.setText("Password field can not be empty!");
+        } else{
+            if(passwordId.getLength() < 8) invalidPasswordId.setText("Password must contain at least 8 characters!");
+            else invalidPasswordId.setText("");
+        }
+
+        if(phoneNumberId.getText().isEmpty()){
+            invalidNumberId.setText("Phone number field can not be empty!");
+        } else{
+            boolean sameNumber = false;
+            for(Customer cust: customers){
+                if(cust.getPhone_number().equals(phoneNumberId.getText())){
+                    sameNumber = true;
+                    break;
+                }
+            }
+            if(sameNumber) invalidNumberId.setText("Phone number is already taken!");
+            else invalidNumberId.setText("");
+        }
+
+    }
+
+    public void initalize(){
+
     }
 }
