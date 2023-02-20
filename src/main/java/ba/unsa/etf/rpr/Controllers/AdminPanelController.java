1bee65297f68962c1b1373e7acfa429d8e1c5c35
ihasicic1 <ihasicic1@etf.unsa.ba>
2023-02-20 22:06:44 +0100

    implemented methods related to customers and bookings

--- src/main/java/ba/unsa/etf/rpr/Controllers/AdminPanelController.java
+++ src/main/java/ba/unsa/etf/rpr/Controllers/AdminPanelController.java
@@ -1,6 +1,10 @@
 package ba.unsa.etf.rpr.Controllers;
 
+import ba.unsa.etf.rpr.business.BookingManager;
+import ba.unsa.etf.rpr.business.CustomerManager;
 import ba.unsa.etf.rpr.business.TourManager;
+import ba.unsa.etf.rpr.domain.Booking;
+import ba.unsa.etf.rpr.domain.Customer;
 import ba.unsa.etf.rpr.domain.Tour;
 import ba.unsa.etf.rpr.exceptions.MyException;
 import javafx.collections.FXCollections;
@@ -10,15 +14,17 @@ import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.*;
 import javafx.scene.control.cell.PropertyValueFactory;
+import javafx.scene.layout.Region;
 import javafx.stage.Stage;
+import javafx.stage.StageStyle;
 
 import java.io.IOException;
+import java.util.Date;
 
 import static javafx.scene.control.PopupControl.USE_COMPUTED_SIZE;
 
 public class AdminPanelController {
 
-    public Button signOutButton;
     public Tab DashboardTabId;
     public Tab CustomersTabId;
     public Tab BookingsTabId;
@@ -33,18 +39,59 @@ public class AdminPanelController {
     public TableView<Tour> toursListId;
     public TabPane TabPaneId;
     public Button logOutId;
+    public TableView<Booking> bookingListId;
+    public TableColumn<Booking, Integer> bookingIdColumn;
+    public TableColumn<Booking, Double> bookingTicketPriceColumn;
+    public TableColumn<Booking, Integer> bookingTourIdColumn;
+    public TableColumn<Booking, Integer> bookingCustomerIdColumn;
+    public TableColumn<Booking, Date> bookingDateColumn;
+    public TableView<Customer> customerListId;
+    public TableColumn<Customer, Integer> customerIdColumn;
+    public TableColumn<Customer, String> customerFirstNameColumn;
+    public TableColumn<Customer, String> customerLastNameColumn;
+    public TableColumn<Customer, String> customerGenderColumn;
+    public TableColumn<Customer, String> customerPhoneNumberColumn;
+    public TableColumn<Customer, String> customerEmailColumn;
+    public TableColumn<Customer, String> customerPasswordColumn;
 
     TourManager tourManager = new TourManager();
+    BookingManager bookingManager = new BookingManager();
+    CustomerManager customerManager = new CustomerManager();
 
 
     public void initialize(){
         idColumn.setCellValueFactory(new PropertyValueFactory<Tour, Integer>("id"));
         destinationColumn.setCellValueFactory(new PropertyValueFactory<Tour, String>("destination"));
         priceColumn.setCellValueFactory(new PropertyValueFactory<Tour, Double>("price"));
-        refreshTour();
+        refreshTours();
+
+        bookingIdColumn.setCellValueFactory(new PropertyValueFactory<Booking, Integer>("id"));
+        bookingTicketPriceColumn.setCellValueFactory(new PropertyValueFactory<Booking, Double>("ticket_price"));
+        bookingTourIdColumn.setCellValueFactory(new PropertyValueFactory<Booking, Integer>("tourId"));
+        bookingCustomerIdColumn.setCellValueFactory(new PropertyValueFactory<Booking, Integer>("customerId"));
+        bookingDateColumn.setCellValueFactory(new PropertyValueFactory<Booking, Date>("date"));
+        refreshBookings();
+
+        customerIdColumn.setCellValueFactory(new PropertyValueFactory<Customer, Integer>("id"));
+        customerFirstNameColumn.setCellValueFactory(new PropertyValueFactory<Customer, String>("first_name"));
+        customerLastNameColumn.setCellValueFactory(new PropertyValueFactory<Customer, String>("last_name"));
+        customerGenderColumn.setCellValueFactory(new PropertyValueFactory<Customer, String>("gender"));
+        customerPhoneNumberColumn.setCellValueFactory(new PropertyValueFactory<Customer, String>("phone_number"));
+        customerEmailColumn.setCellValueFactory(new PropertyValueFactory<Customer, String>("email"));
+        customerPasswordColumn.setCellValueFactory(new PropertyValueFactory<Customer, String>("password"));
+        refreshCustomers();
     }
 
-    void refreshTour(){
+    void refreshCustomers(){
+        try {
+            customerListId.setItems(FXCollections.observableList(customerManager.getAll()));
+            customerListId.refresh();
+        } catch (MyException e) {
+            new Alert(Alert.AlertType.NONE, e.getMessage(), ButtonType.OK).show();
+        }
+    }
+
+    void refreshTours(){
         try{
             toursListId.setItems(FXCollections.observableList(tourManager.getAll()));
             toursListId.refresh();
@@ -52,6 +99,17 @@ public class AdminPanelController {
             new Alert(Alert.AlertType.NONE, e.getMessage(), ButtonType.OK).show();
         }
     }
+
+    void refreshBookings(){
+        try {
+            bookingListId.setItems(FXCollections.observableList(bookingManager.getAll()));
+            bookingListId.refresh();
+        } catch (MyException e) {
+            new Alert(Alert.AlertType.NONE, e.getMessage(), ButtonType.OK).show();
+        }
+    }
+
+
 
     public void dashboardIdAction(ActionEvent actionEvent) {
         TabPaneId.getSelectionModel().select(0);
@@ -70,11 +128,22 @@ public class AdminPanelController {
     }
 
     public void logOutIdAction(ActionEvent actionEvent) throws IOException {
-        Stage s = (Stage) signOutButton.getScene().getWindow();
+        Stage s = (Stage) logOutId.getScene().getWindow();
         s.close();
-        Stage stage = new Stage();
-        Parent root = FXMLLoader.load(getClass().getResource("/fxml/login.fxml"));
-        stage.setScene(new Scene(root, USE_COMPUTED_SIZE, USE_COMPUTED_SIZE));
-        stage.show();
+        openDialog("Log In", "/fxml/login.fxml", null);
+    }
+
+    private void openDialog(String title, String file, Object controller){
+        try {
+            FXMLLoader loader = new FXMLLoader(getClass().getResource(file));
+            loader.setController(controller);
+            Stage stage = new Stage();
+            stage.setScene(new Scene(loader.load(), Region.USE_COMPUTED_SIZE, Region.USE_COMPUTED_SIZE));
+            stage.setTitle(title);
+            stage.initStyle(StageStyle.UTILITY);
+            stage.show();
+        }catch (Exception e){
+            new Alert(Alert.AlertType.NONE, e.getMessage(), ButtonType.OK).show();
+        }
     }
 }
